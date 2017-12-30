b466b01f8539c91a1dd0f9599c1519ce3e858da9
michael bishop <michael.bishop@iohk.io>
2017-12-30 19:39:59 -0400

    more debug restored

--- installers/MacInstaller.hs
+++ installers/MacInstaller.hs
@@ -86,7 +86,7 @@ main = do
 
 makeInstaller :: InstallerConfig -> IO ()
 makeInstaller cfg = do
-  run "ls" [ "-lh", "../release", "../release/darwin-x64", "../release/darwin-x64/DaedalusMantis-darwin-x64", "../release/darwin-x64/DaedalusMantis-darwin-x64/DaedalusMantis.app/Contents/MacOS/" ]
+  run "ls" [ "-lh", "../release", "../release/darwin-x64", "../release/darwin-x64/DaedalusMantis-darwin-x64", "../release/darwin-x64/DaedalusMantis-darwin-x64/DaedalusMantis.app/Contents/MacOS/", "../release/darwin-x64/Daedalus-darwin-x64" ]
   -- TODO, pass this in
   let
     bootstrap_url :: String
